799aeb2b96412579379be9182feb0395f438da57
MusicDev33 <smccowan33@gmail.com>
2021-07-24 21:49:09 -0600

    Update packages, remove type guards

--- src/guards/data-type.guard.ts
+++
@@ -1,9 +0,0 @@
-const ALLOWED = ['downloadSpeed', 'uploadSpeed', 'jitter', 'latency'];
-
-export const DataTypeGuard = (dataType: string): boolean => {
-  if (dataType in ALLOWED) {
-    return true;
-  }
-
-  return false;
-}
