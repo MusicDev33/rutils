c81b2b4e681b3f71340892d1102079b515dd3af3
MusicDev33 <smccowan33@gmail.com>
2021-08-22 22:18:59 -0600

    test commit

---
+++ src/routes/system/schedule.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import { platformSupported } from '@util/sys-support';
+const { exec } = require('child-process-async');
+import { Request, Response } from 'express';
+import amq from 'amqplib';
+
+
+export const scheduleSysUpdatesRoute = (req: Request, res: Response) => {
+
+}
